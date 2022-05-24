678e627a22d9f1f2a33d1416f8775a6919d1eef3
Clayton Mork <mork.clayton3@gmail.com>
2022-05-24 10:57:38 +0900

    Fixed import error when trying to get __version__ from setup.py.

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, find_packages
-from pyevu import __version__
+import codecs
+import os.path
 
 packages = find_packages(
         where='.',
@@ -9,9 +10,23 @@ packages = find_packages(
         ]
 )
 
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    # https://packaging.python.org/en/latest/guides/single-sourcing-package-version/
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 setup(
     name='pyevu',
-    version=__version__,
+    version=get_version("pyevu/__init__.py"),
     description='Python Extensive Vector Utility, is a utility for working with vectors in python.',
     author='Clayton Mork',
     author_email='mork.clayton3@gmail.com',
